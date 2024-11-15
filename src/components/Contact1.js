bb45f03dcf35d6d1cf24993c5cdba94c0b8d4c72
gopikrishnan001 <gokrish2004@gmail.com>
2024-11-15 16:48:37 +0530

    first react app

--- src/components/Contact1.js
+++ src/components/Contact1.js
@@ -8,7 +8,7 @@ export default function Contact1(){
        
         <div className=' flex flex-col  items-center text-white'>
             <h1 className='text-4xl  border-b-4  border-[#1a3951] mb-5 w-[140px] font-bold'>Contact </h1>
-            <p className='pb-5'>if you want to discuss more in detail, please contact me </p>
+            <p className='pb-5'>if you want to discuss more in detail, please contact me.</p>
             <p  className='py-2'><span className='font-bold'>Email:</span>{config.email} </p>
             <p  className='py-2'><span className='font-bold'>phone:</span>{config.phone}</p>
                 </div>
